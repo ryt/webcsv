ee7bcaaa7329bdd5a06dd1743626c16dc9e71c74
Ray <ray@redment.com>
2024-03-16 12:34:39 -0700

    readme add

---
+++ readme.md
@@ -0,0 +1,49 @@
+# webcsv: Simple CSV Web Viewer
+
+Simple python app to view CSV files on the browser. This app uses Flask & Runapp\<Gunicorn\> (ryt/runapp) for Deployment.
+
+## Test/Development Instructions
+
+Run the app with the default Flask development server on port 5000.
+
+```
+$ python3 webcsv.py
+```
+
+## Deployment Instructions
+Create & start a deployment, gunicorn (daemon/process), with app:
+
+```
+$ cd webcsv
+$ ./runapp start
+```
+
+Stop deployment/app process:
+
+```
+$ ./runapp stop
+```
+
+Check running deployment/app process:
+
+```
+$ ./runapp list
+```
+
+Restart deployment/app process:
+
+```
+$ ./runapp restart
+```
+
+If app is not running after restart, check & re-deploy:
+
+```
+$ ./runapp list
+$ ./runapp start
+```
+
+--
+
+<small>Copyright &copy; 2024 Ray Mentose.</small>
+
--- webcsv.py
+++ webcsv.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
-# This app uses Flask & Runapp (ryt/runapp).
+# This app uses Flask & Runapp/Gunicorn (ryt/runapp) for Deployment.
 
-# Create & start gunicorn (daemon/process) with app:
+# Create & start a deployment, gunicorn (daemon/process), with app:
 # $ cd webcsv
 # $ ./runapp start
 
-# Stop app process:
+# Stop deployment/app process:
 # $ ./runapp stop
 
-# Check running app process:
+# Check running deployment/app process:
 # $ ./runapp list
 
-# Restart app process:
+# Restart deployment/app process:
 # $ ./runapp restart
+
+# If not running after restart, check & re-deploy:
+# $ ./runapp list
+# $ ./runapp start
 
 
 from flask import Flask, request
@@ -25,15 +29,39 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/webcsv")
 
-def hello(subpath=None):
+def index(subpath=None):
 
   nl = '\n'
-  html = []
+  s  = ' '
+
+  html = [
+    '<!DOCTYPE html>',
+    '<html>',
+    f'{s*2}<head>',
+    f'{s*4}<meta charset="UTF-8">',
+    f'{s*4}<title>webcsv: Simple CSV Web Viewer</title>',
+    f'{s*4}<style>',
+    f'{s*6}' + f'{nl}{s*6}'.join((
+        'body { margin:0; font:16px sans-serif; }',
+        'h2 { margin:0;padding:8px 16px;background:#ddd;border-bottom:1px solid #ccc; }',
+        'p { margin:0;padding:8px 16px; }',
+      )),
+    f'{s*4}</style>',
+    f'{s*2}</head>',
+    f'{s*2}<body>',
+  ]
 
   getf = request.args.get('f')
 
-  html += ['<h3>webcsv</h3>']
-  html += [f'<p>current value of <b>f</b> is: <u>{getf}</u>']
+  html += [f'{s*4}<h2>webcsv</h2>']
+  html += [f'{s*4}<p>current value of <b>f</b> is: <u>{getf}</u>']
+
+  html += [f'{s*2}</body>']
+  html += ['</html>']
 
   return nl.join(html)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
