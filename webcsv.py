89e2198f2906d3b28ff781a342f4eafbfaa20284
Ray <ray@redment.com>
2024-03-16 13:07:59 -0700

    template added

--- webcsv.py
+++ webcsv.py
@@ -20,46 +20,23 @@
 # $ ./runapp start
 
 
-from flask import Flask, request
+from flask import Flask
+from flask import request
+from flask import render_template
 
 
 
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/webcsv")
+@app.route('/')
+@app.route('/webcsv')
 
 def index(subpath=None):
 
   nl = '\n'
-  s  = ' '
-
-  html = [
-    '<!DOCTYPE html>',
-    '<html>',
-    f'{s*2}<head>',
-    f'{s*4}<meta charset="UTF-8">',
-    f'{s*4}<title>webcsv: Simple CSV Web Viewer</title>',
-    f'{s*4}<style>',
-    f'{s*6}' + f'{nl}{s*6}'.join((
-        'body { margin:0; font:16px sans-serif; }',
-        'h2 { margin:0;padding:8px 16px;background:#ddd;border-bottom:1px solid #ccc; }',
-        'p { margin:0;padding:8px 16px; }',
-      )),
-    f'{s*4}</style>',
-    f'{s*2}</head>',
-    f'{s*2}<body>',
-  ]
-
   getf = request.args.get('f')
 
-  html += [f'{s*4}<h2>webcsv</h2>']
-  html += [f'{s*4}<p>current value of <b>f</b> is: <u>{getf}</u>']
-
-  html += [f'{s*2}</body>']
-  html += ['</html>']
-
-  return nl.join(html)
+  return render_template('template.html', getf=getf)
 
 
 if __name__ == '__main__':
